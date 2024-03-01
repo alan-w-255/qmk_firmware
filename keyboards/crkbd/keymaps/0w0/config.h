f9eab4fffedbef7063df43187865dd112008d3a7
alan-w-255 <1649022341@qq.com>
2024-03-01 20:50:31 +0800

    define SPLIT_USB_DETECT

--- keyboards/crkbd/keymaps/0w0/config.h
+++ keyboards/crkbd/keymaps/0w0/config.h
@@ -30,7 +30,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //#define QUICK_TAP_TERM 0
 #define TAPPING_TERM 200
-#undef SPLIT_USB_DETECT
+#define SPLIT_USB_DETECT
 
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
